e13d569fabfa89a548af2a9a444e04c5169cb27b
Marc94440 <95703747+Marc94440@users.noreply.github.com>
2022-06-24 11:07:02 +0200

    Add files via upload

--- Servo.js
+++ Servo.js
@@ -5,15 +5,15 @@ const Gpio = require('pigpio').Gpio;
 const motor = new Gpio(10, {mode: Gpio.OUTPUT});
 
 let pulseWidth = 1000;
-let increment = 100;
+let increment = 500;
 
 setInterval(() => {
   motor.servoWrite(pulseWidth);
 
   pulseWidth += increment;
-  if (pulseWidth >= 2000) {
-    increment = -100;
+  if (pulseWidth >= 1500) {
+    increment = -500;
   } else if (pulseWidth <= 1000) {
-    increment = 100;
+    increment = 500;
   }
 }, 1000);
