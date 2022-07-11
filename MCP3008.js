c7f39dad499ec9de879120754766d2f44c6eafd3
Marc94440 <95703747+Marc94440@users.noreply.github.com>
2022-07-11 16:30:29 +0200

    Add files via upload

--- MCP3008.js
+++ MCP3008.js
@@ -3,12 +3,12 @@
 */
 'use strict';
 
-var Mcp3008 = require('./mcp3008'),
+var Mcp3008 = require('mcp3008.js'),
     adc = new Mcp3008(),
     out = function (value) {
         console.log("Read", value);
     };
 
 adc.read(0, out);
-adc.poll(0, 400, out);
+adc.poll(0, 2000, out);
 setTimeout(function () { adc.close(); }, 100000);
--- Motor.js
+++ Motor.js
@@ -1,32 +1,27 @@
 'use strict';
 
 const rpio=require('rpio');
+rpio.init({gpiomem: false});
+
 const pinPWM=12;//initialise le pin
 //attention choix du pin entre 12, 32, 33 et 35
 const valMax=1024;
 const div=64;
-const valeur=150;
+//const valeur=v;
 
 rpio.open(pinPWM,rpio.PWM);
 rpio.pwmSetClockDivider(div);//valeur (doit etre une puissance de 2) qui divise 19.2MHz ici on a 128 donc 150KHz
 rpio.pwmSetRange(pinPWM,valMax);//Valeur max de la largeur de l'impulsion
-rpio.pwmSetData(pinPWM,valeur);//valeur du signal
+//rpio.pwmSetData(pinPWM,valeur);//valeur du signal
+//rpio.sleep(30);
 
-rpio.sleep(3);
+var Mcp3008 = require('mcp3008.js'),
+    adc = new Mcp3008(),
+    out = function (value) {
+        console.log("Read", value);
+        rpio.pwmSetData(pinPWM,value);
+    };
 
-/*const rpio=require('rpio');
-
-const pinH=7;
-const pinL=8;
-
-rpio.open(pinH,rpio.OUTPUT);
-rpio.open(pinL,rpio.OUTPUT);
-
-
-rpio.write(pinH,rpio.HIGH);
-rpio.write(pinL,rpio.LOW);
-rpio.sleep(3);
-
-rpio.write(pinH,rpio.LOW);
-rpio.write(pinL,rpio.LOW);
-rpio.sleep(3);*/
+adc.read(0, out);
+adc.poll(0, 2000, out);
+setTimeout(function () { adc.close(); }, 100000);
