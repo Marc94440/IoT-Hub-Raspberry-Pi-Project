67835b4a14e362c513cbb493ff837f7d435dab6c
Marc94440 <95703747+Marc94440@users.noreply.github.com>
2022-07-26 17:11:27 +0200

    Add files via upload

--- Scale.js
+++ Scale.js
@@ -1,9 +1,10 @@
 'use strict';
 const HX711 = require('./hx711');
-const loadCell = new HX711(6, 5, {//6=clk pin 5=datapin
-  continous: 30
+const loadCell = new HX711(6, 5, { //6=clk pin 5=datapin
+  continous: 30,
+  offset : 8300964 //, //A tester (permet de faire le zero)
 });
 
 setInterval(async () => {
-  console.log(loadCell.getLast());
+  console.log(Math.round(loadCell.getLast()/1000)); //on divise pas 1000 pour convertir mg en g et Math.round() arrondit à l'unité
 }, 1000);
