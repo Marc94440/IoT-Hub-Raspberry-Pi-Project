dd0c7c76fff4980124628da72b24008db58ebb24
Marc94440 <95703747+Marc94440@users.noreply.github.com>
2022-08-01 16:57:52 +0200

    Delete DHT-22.js

--- DHT-22.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var sensor = require("node-dht-sensor");
-const rpio=require("rpio")
-    rpio.sleep(1);
-    sensor.read(22, 4, function(err, temperature, humidity) {
-            if (!err) {
-            console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
-            }
-    });
